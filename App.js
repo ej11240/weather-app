0b1b62c193b5dd8daa4a396f0c264c09571fa43b
ej11240 <hwangej1124@gmail.com>
2021-02-26 17:16:42 +0900

    키값 보안 설정(gitignore)

--- App.js
+++ App.js
@@ -3,16 +3,19 @@ import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
-const API_KEY="347c05ed278306843f271a8ea013fe29";
+import Weather from "./Weather";
+import * as config from "./config";
+const API_KEY=config.API_KEY;
 
 export default class extends React.Component {
   state={
     isLoading:true
   }
-  
+
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`);
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
     console.log(data);
+    this.setState({ isLoading: false, condition:"Clear", temp: data.main.temp });
   };
   
   getLocation = async () => {
@@ -22,7 +25,7 @@ export default class extends React.Component {
       const {coords:{latitude, longitude}}=await Location.getCurrentPositionAsync();
       
       this.getWeather(latitude, longitude);
-      this.setState({isLoading:false});
+      
     
     }catch(error){ //사용자가 권한을 주지 않았을 때
       Alert.alert("Can't find you.", "So sad");
@@ -32,7 +35,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render(){
-    const {isLoading}=this.state;
-    return isLoading ?<Loading />:null;
+    const {isLoading, temp, condition}=this.state;
+    return isLoading ?<Loading />:<Weather temp ={temp} condition ={condition} />;
   }
 }
